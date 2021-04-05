083c172433f5be9e919a69993765e89a5636ca34
Hemaja Patoju <phemaja@gmail.com>
2021-04-05 13:15:34 +0530

    mvn existing repo testing commit

---
+++ mvn-bankk/src/main/java/com/ibm/bankk/BalanceException.java
@@ -0,0 +1,14 @@
+package com.ibm.bankk;
+
+@SuppressWarnings("serial")
+public class BalanceException extends Exception {
+
+	public BalanceException() {
+		super();
+	}
+
+	public BalanceException(String arg0) {
+		super(arg0);
+	}
+
+}
